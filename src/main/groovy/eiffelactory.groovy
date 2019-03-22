fb9e195aa281e5de2239fde1b761818ba6eddd6b
Alena Hramyka <alenah@axis.com>
2019-03-22 16:45:18 +0100

    Read afterCreate info
    Change-Id: I513c6f7c67ccddc0fd0985a02aed303f68c870ef

--- src/main/groovy/eiffelactory.groovy
+++ src/main/groovy/eiffelactory.groovy
@@ -1,6 +1,20 @@
-executions {
-    eiffelactory(users: ['anonymous']) {
-        message = '{"status":"ok"}'
-        status = 200
+/**
+ * Handle after create events.
+ *
+ * Closure parameters:
+ * item (org.artifactory.fs.ItemInfo) - the original item being created.
+ */
+import org.artifactory.fs.ItemInfo
+
+storage {
+    afterCreate { item ->
+        toFile(item)
     }
 }
+
+def toFile(ItemInfo item) {
+    File f = new File("/tmp/test1.log")
+    def name = item.getName()
+    f.write name
+}
+
