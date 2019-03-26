13380465996487b6befdb64d3cddff921a1acb65
Martin Winqvist <martinwi@axis.com>
2019-03-26 13:30:52 +0100

    eiffel model suggestion

--- src/main/groovy/eiffelactory.groovy
+++ src/main/groovy/eiffelactory.groovy
@@ -1,3 +1,105 @@
+import groovy.transform.ToString
+
+/**
+ * Represents an Eiffel link
+ * @param type the link type
+ * @param target UUID of target, corresponds to meta.id of the target event
+ */
+@ToString
+class Link  {
+    String type
+    UUID target
+
+    Link(String type, UUID target) {
+        this.type = type
+        this.target = target
+    }
+}
+
+/**
+ * Represents an Eiffel location
+ * @param type The type of location.
+ * @param uri The URI at which the artifact can be retrieved
+ */
+@ToString
+class Location {
+    Type type
+    String uri
+
+    Location(Type type, String uri) {
+        this.type = type
+        this.uri = uri
+    }
+
+    enum Type {
+        ARTIFACTORY,
+        NEXUS,
+        OTHER,
+        PLAIN
+    }
+}
+
+@ToString
+class Source {
+    String domainId
+    String host
+    String name
+    String serializer
+    String uri
+}
+
+@ToString
+class Meta {
+    UUID id = UUID.randomUUID()
+    long time = System.currentTimeMillis()
+    String type
+    String version
+    List<String> tags
+    Source source
+
+    Meta(Map optional = [:], String type, String version) {
+        this.type = type
+        this.version = version
+        this.tags = optional.tags
+        this.source = optional.source
+    }
+}
+
+@ToString
+class EiffelArtifactPublishedEvent {
+    EiffelArtifactPublishedEventMeta meta
+    EiffelArtifactPublishedEventData data
+    List<Link> links
+
+    EiffelArtifactPublishedEvent(EiffelArtifactPublishedEventMeta meta,
+                                 EiffelArtifactPublishedEventData data,
+                                 List<Link> links) {
+        this.meta = meta
+        this.data = data
+        this.links = links
+    }
+}
+
+class EiffelArtifactPublishedEventMeta extends Meta {
+
+    EiffelArtifactPublishedEventMeta(Map optional = [:]) {
+        super(optional, "EiffelArtifactPublishedEvent", "3.0.0")
+    }
+
+    @Override
+    String toString() {
+        return "EiffelArtifactPublishedEvent" + super.toString()
+    }
+}
+
+@ToString
+class EiffelArtifactPublishedEventData {
+    List<Location> locations
+
+    EiffelArtifactPublishedEventData(List<Location> locations) {
+        this.locations = locations
+    }
+}
 /**
  * Handle after create events.
  *
--- src/test/groovy/EiffelactoryTest.groovy
+++ src/test/groovy/EiffelactoryTest.groovy
@@ -1,3 +1,4 @@
+import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
 import org.jfrog.artifactory.client.ArtifactoryClientBuilder
 import spock.lang.Specification
@@ -16,5 +17,29 @@ class EiffelactoryTest extends Specification {
 
         then:
             json.status == 'ok'
+    }
+
+    def 'eiffel artifact published event is correctly constructed'() {
+        setup:
+        def tags = new ArrayList<String>()
+        tags.add("Tag 1")
+        tags.add("Tag 2")
+
+        def source = new Source(host: "some host", domainId: "123")
+
+        def meta = new EiffelArtifactPublishedEventMeta(tags: tags, source: source)
+
+        def locations = new ArrayList<Location>()
+        locations.add(new Location(Location.Type.ARTIFACTORY, "some/artifact/uri"))
+
+        def data = new EiffelArtifactPublishedEventData(locations)
+
+        def links = new ArrayList<Link>()
+        links.add(new Link("ARTIFACT", UUID.fromString("aaaaaaaa-bbbb-5ccc-8ddd-eeeeeeeeeee1")))
+        links.add(new Link("CONTEXT", UUID.fromString("aaaaaaaa-bbbb-5ccc-8ddd-eeeeeeeeeee2")))
+
+        def event = new EiffelArtifactPublishedEvent(meta, data, links)
+
+        println(JsonOutput.prettyPrint(JsonOutput.toJson(event)))
     }
 }
